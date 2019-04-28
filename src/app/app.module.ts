8482632e9daa4f3dabfaed103aa631ff4cfccd65
msosa <m.sosa1127@gmail.com>
2019-04-28 16:28:46 -0400

    fontawesome 4 -> 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faTrashAlt} from '@fortawesome/free-regular-svg-icons';
+import {faFootballBall} from '@fortawesome/free-solid-svg-icons';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -17,10 +21,16 @@ import {LotteryComponent} from './lottery/lottery.component';
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
+		FontAwesomeModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
+	constructor() {
+		// Add an icon to the library for convenient access in other components
+		library.add(faTrashAlt);
+		library.add(faFootballBall);
+	}
 }
