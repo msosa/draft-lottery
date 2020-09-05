8791c616eeb966a71a85facd9e532770a05a921c
msosa <m.sosa1127@gmail.com>
2020-09-05 17:17:45 -0400

    Upgading to angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faTrashAlt} from '@fortawesome/free-regular-svg-icons';
-import {faFootballBall} from '@fortawesome/free-solid-svg-icons';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { faFootballBall } from '@fortawesome/free-solid-svg-icons';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {LotteryComponent} from './lottery/lottery.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LotteryComponent } from './lottery/lottery.component';
 
 @NgModule({
 	declarations: [
@@ -28,9 +27,11 @@ import {LotteryComponent} from './lottery/lottery.component';
 	bootstrap: [AppComponent]
 })
 export class AppModule {
-	constructor() {
+	constructor(library: FaIconLibrary) {
 		// Add an icon to the library for convenient access in other components
-		library.add(faTrashAlt);
-		library.add(faFootballBall);
+		library.addIcons(
+			faTrashAlt,
+			faFootballBall
+		);
 	}
 }
